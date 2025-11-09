b8a0423bcc52ca6adfb478bea13c8fc89e3ae3b5
Michail73 <roc7007@yandex.ru>
2025-11-09 19:34:43 +0100

    9nov

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,16 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
